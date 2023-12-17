1cee852ec64665d0fd47feda09fdc5c9aff8de43
omarosamahu <omarosama255@gmail.com>
2023-12-17 18:30:05 +0200

    Adding main function

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// SOCKETS
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h> // To store address information
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("SOCKETS\n");
+    return 0;
+}
